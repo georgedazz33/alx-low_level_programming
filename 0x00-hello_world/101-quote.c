411dceb94bce2c9408fb8f2ce8f93584fca14e18
George Osas Omoriyekemwen <georgedazzler@gmail.com>
2022-10-13 13:25:34 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-/**
- * main - Entry Point
- *
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59);	
+#include <unistd.h>
+
+/**
+ *
+ * main - Prints out the last part of a quote in the standard error.
+ *
+ * Return: 1 if success.
+ *
+ */
+
+int main(void)
+
+{
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
 }
