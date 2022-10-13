91da6eca6b59291d0848d69388e56a7af74bbfaf
George Osas Omoriyekemwen <georgedazzler@gmail.com>
2022-10-13 11:50:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry Point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (sUCCESS)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); 
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
